2057aba07970377d093b754469ff621ae661a62c
Akhil Devarakonda <adevarakonda555@gmail.com>
2020-06-27 16:16:23 -0700

    week 5 initial commit

---
+++ Solutions/Week5/5_1.cpp
@@ -0,0 +1,5 @@
+
+int get_change(int m) {
+	//write your code here
+	return m / 4;
+}
---
+++ Solutions/Week5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "week5.h"
+
+
+int main() {
+
+	// Switch which problem you want to run from week 5 assignment //
+
+	int problem = 1;
+
+
+	switch (problem) {
+
+	case 1:
+	{
+		int m;
+		std::cin >> m;
+		std::cout << get_change(m) << '\n';
+	}
+	}
+}
