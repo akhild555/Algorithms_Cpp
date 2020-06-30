b5e0d4b03d1fc8960420b37818a390d7eea84f5b
Akhil Devarakonda <adevarakonda555@gmail.com>
2020-06-29 21:44:20 -0700

    5_1 done

--- Solutions/Week5/5_1.cpp
+++ Solutions/Week5/5_1.cpp
@@ -1,5 +1,26 @@
+#include <vector>
+#include <limits>
 
 int get_change(int m) {
-	//write your code here
-	return m / 4;
+	
+	std::vector<int> minNumCoins(m+1);
+	std::vector<int> coins{ 1,3,4 }; // coins available for change
+
+	for (int i = 1; i <= m; i++)
+	{
+		minNumCoins[i] = std::numeric_limits<int>::max();
+		for (size_t j = 0; j < coins.size(); j++)
+		{
+			if (i >= coins[j])
+			{
+				int NumCoins = minNumCoins[i - coins[j]] + 1;
+				if (NumCoins < minNumCoins[i])
+				{
+					minNumCoins[i] = NumCoins;
+				}
+			}
+		}
+	}
+
+	return minNumCoins[m];
 }
